40e997a856c3f34451aeeaae0cff82b02dd23419
ivanretamozo <retamozoi445@gmail.com>
2022-06-30 20:59:33 -0300

    first commit

---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  constructor(private activatedRoute : ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe( ({id}) =>{
+      console.log(id);
+      
+    })
+  }
+
+}
---
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent implements OnInit {
+
+  
+  constructor(private heroesService : HeroesService) { }
+  
+  heroes : Heroe[] =  []
+
+  ngOnInit(): void {
+
+    this.heroesService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
+
+  }
+
+}
